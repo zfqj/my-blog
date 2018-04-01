b86817cf75a2693001c77f6485d054e754b14d35
房启俊 <qijun.fang@chaihuo.org>
2018-04-01 17:18:45 +0800

    haha

---
+++ fuck.js
@@ -0,0 +1,120 @@
+<script>
+
+var fuck={
+    "globalBox":{
+        "a1":"Connect your <label>Main Board</label> to the computer",
+        "a2":"and click confirm to reset it.",
+        "a3":"Confirm",
+        "a4":"Cancel",
+        "a5":"Success!",
+        "a6":"OK",
+        "a7":"Oops...something went wrong.",
+        "a8":"Retry",
+        "a9":"Cancel",
+
+        "b1":"Got it",
+        "b2":"Do not show this again",
+        "b3":"Skip",
+        "b4":"Update Module Matcher",
+        "b5":"Update Module",
+        "b6":"Update Module Matcher",
+        "b7":"Checking for update...",
+        "b8":"Your Module Matcher is up to date.",
+        "b9":"OK",
+        "b10":"Unable to Check for Update.",
+        "b11":"An error accured while checking for update. Please check your network connections.",
+        "b12":"Retry",
+        "b13":"Quit",
+        "b14":"A new version of Module Matcher ",
+        "b15":"is available.",
+        "b16":"Would you like to download and update now?",
+        "b17":"Download and update",
+        "b18":"Remind me later",
+        "b19":"OK",
+        "b20":"Update Module",
+        "b21":"Connect <label class='q-b'>all the Grove Zero Modules</label> (no identical modules) you want to update to your Main Board, then connect your Main Board to the computer and scan for updates. ",
+        "b22":"Scan",
+        "b23":"Back",
+        "b24":"Scanning...",
+        "b25":"The modules you connected are all up-to-date.",
+        "b26":"Module</span><span class='w2'>Current Version</span><span class='w3'>Latest Version</span><span class='w4 flex-1'>Status",
+        "b27":"Grove Zero Modules can be updated <label class='q-m'>only one at a time</label> using the Main Board. To update a module, be sure it is the only module connected to the Main Board.",
+        "b28":"You just completed your update.",
+        "b29":"Back",
+        "b30":"No module has been detected.",
+        "b31":"Please reconnect the modules and try again.",
+        "b32":"Retry",
+        "b33":"Quit",
+        "b34":"New Features",
+        "b35":"Note: You need to update your modules to use the new features.",
+        "b36":"Do not show this again",
+
+        "c1":"Welcome! Young Maker!",
+        "c2":"Module Matcher is a super easy tool to help you make simple projects by matching the actions of an <label style='color: #00ACDB;'>Input Module</label> and the reactions of an <label style='color: #E04B49;'>Output Module</label>. Like this:",
+        "c3":"Step 1. Select an <label style='color: #00ACDB;'>Input</label> Module",
+        "c4":"Step 2. Select An <label style='color: #E04B49;'>Output</label> Module",
+        "c5":"Step 3. Set the Actions of Input",
+        "c6":"Step 4. Set the Reactions of Output",
+        "c7":"Step 5. Click ‘Match’ to Complete",
+        "c":""
+    },
+    "headModule":{
+        "a1":"Reset Main Board",
+        "a2":"Check for Updates",
+        "a3":"About Software",
+
+        "b1":"New Project",
+        "b2":"Import Project",
+        "b3":"Save Project",
+        "b4":"Getting Started",
+        "b5":"Help",
+        "b":"",
+        "b":""
+    },
+    "selectOne":{
+        "a1":"Update Available.",
+        "a2":"You can find the tutorial here."
+    },
+    "selectTwo":{
+        "a1":"Cold: when temperature is below 10℃",
+        "a2":"Hot: when temperature is above 40℃",
+        "a3":"Dark: when light level is below 50 lux",
+        "a4":"Light: when light level is above 200 lux",
+        "a5":"Quiet: when sound level is below 50",
+        "a6":"Loud: when sound level is above 200"
+    },
+    "match":{
+        "a1":"Match the event that happens on the input/sensor module.",
+        "a2":"",
+        "a3":"Sorry, a buzzer cannot be used to read sensor data.",
+        "a4":"Add modules at the left to get started",
+
+        "b1":"Display a pixel image on LED Matrix for a given duration.",
+        "b2":"Display numbers on LED Matrix for a given duration.",
+        "b3":"Display text on LED Matrix for a given duration.",
+        "b4":"Display a led bar level on LED Matrix for a given duration.",
+        "b5":"Play tones on buzzer for a given beat.",
+        "b6":"Ring tones on buzzer for a given duration.",
+        "b7":"Play melody on buzzer for a given duration.",
+        "b8":"Get the temperature(℃) from sensor module and do something on the output module.",
+        "b9":"Get the light level from sensor module and do something on the output module.",
+        "b10":"Get the sound level from sensor module and do something on the output module.",
+        
+        "c1":"Display",
+        "c2":"For",
+        "c3":"Buzzer",
+        "c4":"For"
+    },
+    "bottomModule":{
+        "a1":"Match Complete!",
+        "a2":"Oops... Please connect your",
+        "a3":"mainboard to the computer",
+        "a4":"to complete the matches."
+    }
+};
+
+
+    for(var key in fuck){
+        console.(key);
+    }
+</script>
